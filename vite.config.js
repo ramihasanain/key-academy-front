d1b662d4d9b85ceb035229f60a2c7c61506222aa
Rami Hasanain <rami.x.hasanain@gmail.com>
2026-04-20 19:41:27 +0300

    chore: add pdfjs-dist alias to vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'pdfjs-dist': 'pdfjs-dist/legacy/build/pdf.mjs',
+    }
+  },
   server: {
     port: 5173,
     open: true
